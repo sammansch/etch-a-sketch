56943987f85e896b5a8493965c3cba9f3837316b
Sam Mansch <s.mansch@gmail.com>
2022-03-10 16:10:05 -0600

    Add loop to make 16x16 grid of boxes via DOM manipulation and initial hover functionality

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-//1. create 16x16 grid of square divs with JS
+//2. create 16x16 grid of square divs with JS
 
 const container = document.querySelector('.main');
 
 //loop to create 16 "game cells"
-for (let i = 0; i < 16; i++){
-    const box = document.createElement('div');
-    box.className = "game-cell";
-    box.style.backgroundColor = 'lightgray';
-    box.textContent = i+1;
-    container.appendChild(box);
-}
+let x = 0;
+do{
+    for (let i = 0; i < 16; i++){
+        const box = document.createElement('div');
+        box.className = "game-cell";
+        //box.style.backgroundColor = 'lightgray';
+        box.textContent = i+1;
+        container.appendChild(box);
+    }
+    x++
+} while (x < 16)
+
+
+//3. Set up a “hover” effect so that the grid divs change color 
+//when your mouse passes over them, leaving a (pixelated) trail 
+//through your grid like a pen would.
