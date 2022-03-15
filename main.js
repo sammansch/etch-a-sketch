228d5dbd741dfe8f3b70b570228f226871748f24
Sam Mansch <s.mansch@gmail.com>
2022-03-15 08:42:11 -0500

    Use CSS Grid to generate cells on board

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-//2. create 16x16 grid of square divs with JS
 
+let number =  window.prompt('Enter a number 1 to 100:');
 const container = document.querySelector('.main');
 
-//loop to create 16 "game cells"
-let x = 0;
-do{
-    for (let i = 0; i < 16; i++){
+createBoard(number, number);
+
+//create grid of square divs (number x number)
+function createBoard(rows, cols) {
+
+    container.style.setProperty('--grid-rows', rows); 
+    container.style.setProperty('--grid-cols', cols); 
+    for (let r = 0; r < rows; r++){
+        createCells(cols);
+    }
+}
+
+function createCells(number) {
+    for (let i = 0; i < number; i++){
         const box = document.createElement('div');
         box.className = "game-cell";
-
         container.appendChild(box);
     }
-    x++
-} while (x < 16);
+}
 
 //cells change color as the cursor passes through 
 container.addEventListener('mouseover', function(event) {
@@ -23,7 +31,7 @@ container.addEventListener('mouseover', function(event) {
 const header = document.querySelector('.header');
 const resetBtn = document.createElement('button');
 resetBtn.className = "reset-grid";
-resetBtn.textContent = "Clear Grid";
+resetBtn.textContent = "Reset";
 header.appendChild(resetBtn);
 
 resetBtn.addEventListener('click',() => location.reload());
